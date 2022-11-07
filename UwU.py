9df834a90190a0b4f663755ee761bd6504bba02a
BOISMENU Baptiste <bboismenu@gaming.tech>
2022-11-07 18:44:27 +0100

    Exemple

---
+++ UwU.py
@@ -0,0 +1,21 @@
+#DEBUT
+r = 12000
+s = 1250
+e = 10
+rh = 230
+
+assertionFinale = (( (365 * 3) / (24 - (16 - 8)) ) * (rh) > r) == (e * s < r)
+
+
+assertionFinaleDeux = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) == (e * s < r)
+assertionFinaleDeux = apt3 == apt4
+apt3 = (1095 / 0) * 230 > 12000 #FALSE
+apt4 = 12500 < 12000 #FALSE
+assertionFinaleDeux = FAlSE == FALSE #TRUE
+
+def retournerSixPlusTrois():
+    return 6 + 3
+def retournerSixPlusX(x):
+    return 6 + x
+
+#FIN
