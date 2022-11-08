ced4557e339576946c1c2926a629a7f75fb42aff
BOISMENU Baptiste <bboismenu@gaming.tech>
2022-11-08 17:44:36 +0100

    Correction des bugs dans Exo.py

--- Exo.py
+++ Exo.py
@@ -8,11 +8,31 @@ def sub(x,y):
 def multiply(x,y):
     return x * y
 
+#Définir une fonction qui retourne le résultat de la division de x par y
 def divide(x,y):
-    return x / y
+    #Vérifie si le diviseur est 0
+    if y == 0:
+        #Afficher un message d'erreur
+        print("Wsh tfq tu divises pas par 0 où je te démarre")
+        #Retourner vide
+        return
+    #Sinon, effectuer la division
+    else:
+        #Retourner le résultat de la division
+        return x / y
 
+#Définir une fonction qui retourne le reste de la division euclidienne de x par y
 def modulo(x,y):
-    return x % y
+    #Vérifie si le diviseur est 0
+    if y == 0:
+        #Afficher un message d'erreur
+        print("Wsh tfq tu divises pas (euclidiennement) par 0 où je te démarre")
+        #Retourner vide
+        return
+    #Sinon, effectuer la division euclidienne
+    else:
+        #Retourner le reste de la division euclidienne
+        return x % y
 
 def salaireNet(salaireBrut, coef):
     return salaireBrut * coef                   #sa c fo wsh
