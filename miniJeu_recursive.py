7ff73faafe844b89af6e6e91992f2c93b6539489
BOISMENU Baptiste <bboismenu@gaming.tech>
2022-11-08 19:58:45 +0100

    tf ?

---
+++ miniJeu_recursive.py
@@ -0,0 +1,8 @@
+#Définir la fonction miniJeuRecur qui affiche un message de victoire avec le nombre d'essaie lorsque input renvoi le caractère passer en paramètre (charChoisis, nbTentativesPassées)
+    #Inititialiser nbTentatives à 1
+    #Stocker la résultante d'input dans la variable essaie
+    #Si essaie égal charChoisis, Alors...
+        #Retourner nbTentatives
+    #Sinon : essaie différent de charChoisis, Alors...
+        #Stocker le résultat de nbTentativesPassées + la résultante de l'appel de miniJeuRecur(charChoisis, nbTentatives)
+    
