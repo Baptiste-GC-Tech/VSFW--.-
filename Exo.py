f12829f147d9afd84b870fe70bb4f89e2a7a0bc1
BOISMENU Baptiste <bboismenu@gaming.tech>
2022-11-10 12:48:13 +0100

    Tentative Conway + Fibonacci

--- Exo.py
+++ Exo.py
@@ -127,9 +127,92 @@ def returnString(string):
 #Exo bonus yay :
     #Suite de Fibonacci en fonction d'uen valeur de départ et du nombre d'occurence (max 10)
 
-#Définir fibo(startingValue, nbTurn) qui affiche les nbTurn première valeurs depuis startingValue avec la règle de Fibonacci
-    #Si nbTurn > 10, Alors...
+#Définir fibo(startingValue, nbIterations) qui affiche les nbTurn premières valeurs depuis startingValue avec la règle de Fibonacci
+    #Initialiser nbTurn à nbIterations
+    #Si nbIterations > 10, Alors...
         #Gronder l'utilisateur (povre CPU smh) et lui dire qu'on en fera que 10
-        #
+        #Assigner à nbTurn la valeur 10
+    #Initialiser une variable fibo à startingValue
+    #Initialiser une variable fiboPrev à 0
+    #Initialiser une variable suiteFibo à str(startingValue)
+    #Initialiser une variable i à 1
+    #Tant que i < nbTurn, Alors...
+        #Assigner à fibo la valeur fibo + fiboPrev
+        #Assginer à fiboPrev la valeur fibo - fiboPrev
+        #Assigner à suiteFibo la valeur suiteFibo + ", " + str(fibo)
+        #Incrémenter i de 1
+    #Afficher suiteFibo
+
+
+
+#On admet la fonction random() qui génère des nombre pseudo aléatoire
+#On admet la fonction os() qui permet d'éxecuter des commandes de la console windows
+
+#Définir conway(refreshRate, duration, size) qui simule le jeu de la vie (les ' ' représente une cellules morte, les '█' représente les cellules vivantes)
+    #Initialiser un tableau 2D tel que : conwayBoard =  [[' '] * size] * size
+    #Initialiser simulatedTicks à 0
+    #Initialiser une variable xCoord à 0 (pour parcourir le tableau 2D horizontalement)
+    #Initialiser une variable yCoord à 0 (pour parcourir le tableau 2D verticalement)
+    #Initialiser une variable xCoordNeighbour à 0 (pour faire le tour de la cellule séléctionné horizontalement)
+    #Initialiser une variable yCoordNeighbour à 0 (pour faire le tour de la cellule séléctionné verticalement)
+    #Initialiser une variable randomNb à None
+    #Initialiser une liste selectedCell à [None, None]
+    #Initialiser une variable nbNeighbours à None
+
+    #Tant que xCoord < size, Alors...
+        #Tant que yCoord < size, Alors...
+            #Assigner à randomNb la résultante de la fonction random.randint(0,99)
+            #Si randomNb < 15, Alors...
+                #Assigner conwayBoard[xCoord][yCoord] à '█'
+            #Sinon : randomNb est différent de 0, Alors...
+                #Assigner conwayBoard[xCoord][yCoord] à ' '
+            #Incrémenter yCoord de 1
+        #Incrémenter xCoord de 1
+        #Assigner à yCoord la valeur 0
+    
+    #Appeler la fonction os.system('cls') pour effacer le contenu de la console
+    #Appeler la fonction print('\n')
+    #Appeler la fonction print(conwayBoard)
+    
+    #Tant que simulatedTicks < duration, Alors...
+        #Assigner à xCoord la valeur -1
+        #Assigner à yCoord la valeur -1
+        #Tant que xCoord < size, Alors...
+            #Assigner à selectedCell[0] la valeur xCoord
+            #Tant que yCoord < size, Alors...
+                #Assigner à selectedCell[1] la valeur yCoord
+                #Tant que xCoordNeighbour < 2, Alors...
+                    #Tant que yCoordNeighbour < 2, Alors...
+                        #Si xCoordNeighbour est différent de 2 ET yCoordNeighbour différent de 2, Alors...
+                            #Si conwayBoard[selectedCell[0] + xCoordNeighbour][selectedCell[1] + yCoordNeighbour] est égal à '█', Alors...
+                                #Incrémenter nbNeighbours de 1
+                        #Assigner à nbNeighbours la valeur 0
+                        #Incrémenter yCoordNeighbour de 1
+                    #Incrémenter xCoordNeighbour de 1
+                #Incrémenter yCoord de 1
+        #Incrémenter xCoord de 1
+        #Assigner à yCoord la valeur 0
+
+        #Si xCoord est égal à size - 1, Alors...
+            #Incrémenter simulatedTicks de 1
+
+        #Si simulatedTicks % refreshRate est égal à 0, Alors...
+            #Appeler la fonction os.system('cls') pour effacer le contenu de la console
+            #Appeler la fonction print('\n')
+            #Appeler la fonction print(conwayBoard)
+    
+    #Appeler la fonction os.system('cls') pour effacer le contenu de la console
+    #Appeler la fonction print('\n')
+    #Appeler la fonction print(conwayBoard)
+
+def gT(L):
+    return [[(str(x) + '_' + str(y)) for x in range(L)] for y in range(L)]
+
+def disT(table):
+    for i in table:
+        print(i)
+
+conwayBoard = gT(5)
+disT(conwayBoard)
 
 #FIN
